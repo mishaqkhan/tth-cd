8a459caf94f20cf732d859343bb06ab6f683169c
mishaqkhan <m.ishaqkhan1993@gmail.com>
2020-06-04 21:21:40 +0500

    Updates

---
+++ precache-manifest.c8a1a421f9a56da20f27eb2b0fd4a1d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0355897ae2360fabf60a",
+    "url": "/tth-cd/static/css/main.60c34f9b.chunk.css"
+  },
+  {
+    "revision": "0355897ae2360fabf60a",
+    "url": "/tth-cd/static/js/main.0355897a.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/tth-cd/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "cdd2a4d5afa12688340c",
+    "url": "/tth-cd/static/js/runtime~main.cdd2a4d5.js"
+  },
+  {
+    "revision": "2f1b9d67b345a559e0677b079ea21e60",
+    "url": "/tth-cd/index.html"
+  }
+];
