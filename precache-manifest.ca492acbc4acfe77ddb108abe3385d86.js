e1023c87ead51aa10dd22df92aeb10637a9d43ca
mishaqkhan <m.ishaqkhan1993@gmail.com>
2020-06-04 21:37:48 +0500

    Updates

---
+++ precache-manifest.ca492acbc4acfe77ddb108abe3385d86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "85f6b9a2b5be76d2c873",
+    "url": "/tth-cd/static/css/main.60c34f9b.chunk.css"
+  },
+  {
+    "revision": "85f6b9a2b5be76d2c873",
+    "url": "/tth-cd/static/js/main.85f6b9a2.chunk.js"
+  },
+  {
+    "revision": "25a97381f2fd20d18f35",
+    "url": "/tth-cd/static/js/1.25a97381.chunk.js"
+  },
+  {
+    "revision": "cdd2a4d5afa12688340c",
+    "url": "/tth-cd/static/js/runtime~main.cdd2a4d5.js"
+  },
+  {
+    "revision": "0caeccc51585e77a2f7a9d08b67cf942",
+    "url": "/tth-cd/index.html"
+  }
+];
