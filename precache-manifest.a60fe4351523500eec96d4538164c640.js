b37f0df0ed51017b08ab917fcb2a2623ddafa6bb
mishaqkhan <m.ishaqkhan1993@gmail.com>
2020-06-04 21:39:35 +0500

    Updates

--- precache-manifest.a60fe4351523500eec96d4538164c640.js
+++ precache-manifest.a60fe4351523500eec96d4538164c640.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "85f6b9a2b5be76d2c873",
+    "revision": "6d5e8999daa7c969f8db",
     "url": "/tth-cd/static/css/main.60c34f9b.chunk.css"
   },
   {
-    "revision": "85f6b9a2b5be76d2c873",
-    "url": "/tth-cd/static/js/main.85f6b9a2.chunk.js"
+    "revision": "6d5e8999daa7c969f8db",
+    "url": "/tth-cd/static/js/main.6d5e8999.chunk.js"
   },
   {
     "revision": "25a97381f2fd20d18f35",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/tth-cd/static/js/runtime~main.cdd2a4d5.js"
   },
   {
-    "revision": "0caeccc51585e77a2f7a9d08b67cf942",
+    "revision": "4d51ee0e13747db90b936b9c06b27dac",
     "url": "/tth-cd/index.html"
   }
 ];
